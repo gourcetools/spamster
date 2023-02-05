4f068c30a4f83b11038c68664026d4685192bac3
gourcetools <120996278+gourcetools@users.noreply.github.com>
2023-02-05 16:02:39 +0100

    Multi spamster TOR by default

--- src/scripts/send-kind1-public/multi-spamster.sh
+++ src/scripts/send-kind1-public/multi-spamster.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 fulldir=$(pwd)
 basename="${fulldir##*/}"
 echo "$basename"
@@ -5,7 +7,6 @@ mkdir -p ../../../../multi-spamster
 rm -f -r ../../../../multi-spamster/*
 echo "== Enter how many bots you want to create:"
 read count
-i=0
 while (( i++ < $count )); do
   cp -r "../../../../spamster" "../../../../multi-spamster/spamster$i"
 done
@@ -13,10 +14,42 @@ echo " == Done making $count bots"
 echo " == Lets start them.."
 cd ../../../../multi-spamster
 ls
+
+
+number=1
+port=9050
+control_port=9051
+
+
+
+
 for d in ./* ; do
-    echo "dir: $d"	
-    cd $d/src/scripts/$basename
-    x-terminal-emulator -e ./send-loop.sh &
-    cd ../../../../
+  echo "Im inside: $d"
+  cd $d/src/scripts/$basename
+  password=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+  hashed_password=$(tor --hash-password "$password")
+mkdir -p etc/
+mkdir -p tordata$number/
+ls
+  echo "SOCKSPort $port" > 	etc/torsocks.conf$number
+  echo "ControlPort $control_port" >> etc/torsocks.conf$number
+  echo "HashedControlPassword $hashed_password" >> etc/torsocks.conf$number
+  echo "DataDirectory" tordata$number >> etc/torsocks.conf$number
+  echo "ExitNodes *" >> etc/torsocks.conf$number
+  
+  echo "$port" > SOCKSPORT
+  echo "$control_port" > CONTROLPORT
+  echo "$number" > NUMBER
+
+x-terminal-emulator -e "bash -c './send-loop.sh; bash'" &
+
+
+  cd ../../../../
+  let "number+=1"
+  let "port+=10"
+  let "control_port+=10"
 done
 cd ../spamster/src/scripts/multi-spamster
+
+
+
--- src/scripts/send-kind1-public/send-loop.sh
+++ src/scripts/send-kind1-public/send-loop.sh
@@ -1,28 +1,67 @@
-succes=0
+number=$(cat NUMBER)
+port=$(cat SOCKSPORT)
+control_port=$(cat CONTROLPORT)
+echo "== TOR: "
+echo "== Port: $port"
+echo "== Control port:$control_port"
+
+echo "== Launching tor"
+x-terminal-emulator -e tor -f "etc/torsocks.conf$number" -SocksPort $port &
+
+# Let tor start properly.
+sleep 30
+echo " "
+
+
+IP=$(curl --socks5-hostname 127.0.0.1:$port -s https://check.torproject.org | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | head -n 1)
+echo $IP
+
+
+success=0
 failed=0
-IP=$($tor wget -qO- http://ipecho.net/plain)
+timedout=0
+
 while true
 do
 privkey=$(shuf -n 1 ../../../config/privkeys-list.txt)
 message=$(shuf -n 1 ../../../config/messages-list.txt)
 relay=$(shuf -n 1 ../../../config/relays-list.txt)
+
+
+
+sleep 1
 clear
+
 echo "		┌───────────────────────────────┐"
-echo "		│ *SPAMSTER IS RUNNING*		 "
+echo "		│ *SPAMSTER $number IS RUNNING*	:)	 "
 echo "		│ * IP: "$IP"     		 "
 echo "		├───────────────────────────────┤"
-echo "		│ Total send: "$succes"		 "
+echo "		│ Total succes: "$success"		 "
 echo "		│ Total failed : "$failed"	 "
+echo "		│ Total timed out : "$timedout"	 "
 echo "		└───────────────────────────────┘"
+echo " "
 echo "Relay: $relay"
 echo "Message: $message"
-$tor timeout 5s nostr-tool -p "$privkey" -r "$relay" text-note -c "$message" 
-if [ $? -eq 124 ]
-then
+
+output=$(timeout 5s bash -c "nostril --envelope --sec \"$privkey\" --content \"$IP\" | websocat --socks5 127.0.0.1:$port $relay")
+echo $output 
+  if [ $? -eq 124 ]
+  then
+    timedout=$(($timedout+1))
+    echo "❌ Timed out."
+  elif echo "$output" | grep -q "OK"; then
+    success=$(($success+1))
+    echo "✅ Success."
+
+  else
   failed=$(($failed+1))
-  echo "❌ Failed."
-else
-  succes=$(($succes+1))
-  echo "✅ Succes."
-  fi	
+    echo "❌ FAILED.: $output"
+  fi		
 done
+
+
+
+
+
+
