65162b8fc9f6537c89a5b48223e60ccf5e9dca7d
gourcetools <120996278+gourcetools@users.noreply.github.com>
2023-02-06 04:36:15 +0100

    Fix reset

--- src/scripts/reset/reset.sh
+++ src/scripts/reset/reset.sh
@@ -1,16 +1,23 @@
+" ============================="
+echo " == SPAMSTER RESET == "
+" ============================="
 cd ../../
+echo " = Purging tmp folder"
 cd tmp
 rm -f *
 cd ..
+echo " = Purging pubkeys folder"
 cd keys/pubkeys
 rm -f *
 cd ..
+echo " = Purging privkeys folder"
 cd privkeys
 rm -f *
-cd ..
-cd names
-rm -f *
-cd ../..
-cd json
+echo " = Purging json"
+cd ../../json
 rm -f *
 cd ../scripts/reset
+" ============================="
+echo " == SPAMSTER RESET DONE == "
+" ============================="
+sleep 5
