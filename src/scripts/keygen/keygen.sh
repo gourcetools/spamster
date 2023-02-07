00bb7c1529edd43f8b5b3c678013265c1efe8fac
gourcetools <120996278+gourcetools@users.noreply.github.com>
2023-02-07 05:24:37 +0100

    Fix names if empty space

--- src/scripts/keygen/keygen.sh
+++ src/scripts/keygen/keygen.sh
@@ -1,23 +1,32 @@
 #!/bin/bash
 # keygen.sh
+
+# Start timer
+start_time=$(date +%s)
+
 num_processors=$(grep -c ^processor /proc/cpuinfo)
 xarg_p=$((num_processors - 1))
 echo $xarg_p
+
 # Create directories
 mkdir -p ../../tmp
 mkdir -p ../../json
 mkdir -p ../../keys/pubkeys
 mkdir -p ../../keys/privkeys
 mkdir -p ../../keys/names
+
 # Delete old files
 rm -f ../../tmp/*
 rm -f ../../keys/pubkeys/*
 rm -f ../../keys/privkeys/*
+
+# Replace spaces with dashes 
+sed -i 's/ /-/g' ../../../config/names-list.txt
+
 # Get the number of lines in names-list.txt
 lines=$(wc -l < "../../../config/names-list.txt")
-# Start timer
-start_time=$(date +%s)
-# Run command for each name in names-list.txt
+
+# Run keygen command for each name in names-list.txt
 cat ../../../config/names-list.txt | xargs -I {} -P $xarg_p sh -c '
     echo "Generating keypair for {}"
     keypair=$(clust generate-keypair)
@@ -30,10 +39,13 @@ cat ../../../config/names-list.txt | xargs -I {} -P $xarg_p sh -c '
 cd ../../keys/privkeys
 find . -type f -name '*.txt' -print0 | sort -z | xargs -0 cat > ../../../config/privkeys-list.txt
 cd ../../scripts/keygen
+
 # End timer
 end_time=$(date +%s)
+
 # Calculate total time
 total_time=$((end_time - start_time))
+
 # Print total time and number of lines
 echo "= Done :)
 = Created $lines accounts in $total_time seconds."
