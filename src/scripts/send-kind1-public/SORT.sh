a04ea8b1989c4215562779e9bd2d1b4a94f0e14a
gourcetools <120996278+gourcetools@users.noreply.github.com>
2023-02-07 02:59:10 +0100

    Split SORT and change echo of START-SPAM

--- src/scripts/send-kind1-public/SORT.sh
+++ src/scripts/send-kind1-public/SORT.sh
@@ -1,43 +1,25 @@
 if [ $? -eq 124 ]; then
-  timedout=$((timedout + 1))
-  echo " ⏰ TIMED OUT."
-  echo " Removing $relay  from relay list"
+  echo " ❌ Removing $relay  from relay list"
   grep -v "$relay" ../../../config/relays-list.txt > tmp_file
   mv tmp_file ../../../config/relays-list.txt
-
-
-elif echo "$output" | grep -q "true"; then
-  success=$((success + 1))
-  echo $relay >> ok-relays.txt
-  echo "$relay replied:"
-  echo " $output "
-  echo " "
-  echo " ✅ Success."
-  
-  
-elif echo "$output" | grep -q "created"; then
-  success=$((success + 1))
-  echo $relay >> ok-relays.txt
-  echo "$relay replied:"
-  echo " $output "
-    echo " "
-  echo " ✅ Success."
-  
   
 elif echo "$output" | grep -q "false"; then
-  failed=$((failed + 1))
-  echo "$relay replied: "$output""
-  echo " "
-  echo " ❌ Failed, FALSE."
+
   echo " ❌ Removing $relay  from relay list"
   grep -v "$relay" ../../../config/relays-list.txt > tmp_file
   mv tmp_file ../../../config/relays-list.txt
 
+elif echo "$output" | grep -q "true"; then
+
+  echo $relay >> ok-relays.txt
+
+  
+elif echo "$output" | grep -q "created"; then
+
+  echo $relay >> ok-relays.txt
+
+
 else
-  noreply=$((noreply + 1))
-  echo " Reply from $relay:"
-  echo "  "$output" "
-  echo " 😟 No reply / error from $relay. likelly failed."
   echo " ❌ Removing $relay  from relay list"
   grep -v "$relay" ../../../config/relays-list.txt > tmp_file
   mv tmp_file ../../../config/relays-list.txt
@@ -47,13 +29,8 @@ fi
 
 if [ -f ok-relays.txt ]; then
   sort ok-relays.txt | uniq > temp.txt && mv temp.txt ok-relays.txt
-  echo "======================================================================================="
-  echo " 	✅ RELAYS THAT ACCEPTED EVENTS DURING THIS SESSION: ✅"
-  cat ok-relays.txt
   cat ok-relays.txt >> ../../../config/relays-list.txt
   sort ../../../config/relays-list.txt | uniq > temp.txt && mv temp.txt ../../../config/relays-list.txt
 else
   echo " "
 fi
-
-done
--- src/scripts/send-kind1-public/START-SPAM.sh
+++ src/scripts/send-kind1-public/START-SPAM.sh
@@ -11,70 +11,66 @@ TIMEOUT=$(cat ../../../config/TIMEOUT)
 POW=$(cat ../../../config/POW-EVENTS)
 NUMBER=$(cat NUMBER)
 PORT=$(cat PORT)
-
+echo " " > ok-relays.txt 
 
 # IF 1 
 if [ "$USETOR" == "yes" ]
   then
     echo " == You asked me to use tor, let me start it first..."
     source ./START-TOR.sh
-       # Tor loop
+       # TOR LOOP
 	while true
 	do
 		privkey=$(shuf -n 1 ../../../config/privkeys-list.txt)
 		message=$(shuf -n 1 ../../../config/messages-list.txt)
 		relay=$(shuf -n 1 ../../../config/relays-list.txt)
 		countrelayslist=$(wc -l < ../../../config/relays-list.txt)
-		echo " "
-		echo " "
-		echo " "
+		
 		clear
-		echo "============================================================================================"
-		echo "=== 🤖 SPAMSTER N°"$NUMBER" IS RUNNING | RELAYS: "$countrelayslist" "
+		echo "============================================================================="
+		echo "=== 🤖 SPAMSTER N°"$NUMBER" IS RUNNING | RELAYS IN LIST: "$countrelayslist" "
 		echo "=== 🧅 TOR-IP: "$IP" ON PORT:$PORT "
-		echo "=== 📨 TOTAL SEND :"$total" | ✅ OK FOR SURE::"$success" | ❓ NO REPLY (MAYBE OK) :"$noreply" | ❌ FAILED FOR SURE:"$failed" | ⏰ TIMED-OUT:"$timedout"" 
-		echo "============================================================================================"
-		echo "= SENDING TO RELAY: "$relay" | MESSAGE: "$message" "
-		echo ""
-		total=$((total + 1))
-		output=$(timeout "$TIMEOUT"s bash -c "nostril --envelope --sec '$privkey' --content '${message//\'/\'\\\'\'}' | websocat --socks5 127.0.0.1:$PORT $relay")
+		echo "=== 📨 TOTAL SEND :"$total" | ✅ OK FOR SURE:"$success""
+		echo "=== ❓ NO REPLY (MAYBE OK) :"$noreply" | ❌ FAILED FOR SURE:"$failed" | ⏰ TIMED-OUT:"$timedout"" 
+		echo "============================================================================="
+		echo "=== ✅ RELAYS THAT ACCEPTED EVENTS DURING THIS SESSION:   "
+		cat ok-relays.txt
+		echo "============================================================================="
+		echo "= RELAY: "$relay" "
+		echo "= MESSAGE: "$message" "
+	        total=$((total + 1))
+		output=$(timeout "$TIMEOUT"s bash -c "nostril --envelope --sec '$privkey' --content '${message//\'/\'\\\'\'}' --pow "$POW" | websocat --socks5 127.0.0.1:$PORT $relay")
+		source ./SORT.sh
 		source ./COUNT.sh
 	done
- 
 
 else
  
 # NON-TOR LOOP
-
 while true
 do
 privkey=$(shuf -n 1 ../../../config/privkeys-list.txt)
 message=$(shuf -n 1 ../../../config/messages-list.txt)
 relay=$(shuf -n 1 ../../../config/relays-list.txt)
 countrelayslist=$(wc -l < ../../../config/relays-list.txt)
-echo " "
-echo " "
-echo " "
-echo " "
-
 
 clear
-echo "============================================================================================"
-echo "=== 🤖 SPAMSTER N°"$NUMBER" IS RUNNING | RELAYS: "$countrelayslist""
+echo "============================================================================="
+echo "=== 🤖 SPAMSTER N°"$NUMBER" IS RUNNING | RELAYS IN LIST: "$countrelayslist""
 echo "=== 🌐 YOUR IP: "$IP""
-echo "=== 📨 TOTAL SEND :"$total" | ✅ OK FOR SURE::"$success" | ❓ NO REPLY (MAYBE OK) :"$noreply" | ❌ FAILED FOR SURE:"$failed" | ⏰ TIMED-OUT:"$timedout"" 
-echo "============================================================================================"
-echo "= SENDING TO RELAY: "$relay" | MESSAGE: "$message" "
-echo "= . . . . . . . . . . . . . . . . . . . . . . . . . "
+echo "=== 📨 TOTAL SEND :"$total" | ✅ OK FOR SURE:"$success""
+echo "=== ❓ NO REPLY (MAYBE OK) :"$noreply" | ❌ FAILED FOR SURE:"$failed" | ⏰ TIMED-OUT:"$timedout"" 
+echo "============================================================================="
+echo "=== ✅ RELAYS THAT ACCEPTED EVENTS DURING THIS SESSION:  "
+cat ok-relays.txt
+echo "============================================================================="
+echo "= RELAY: "$relay" "
+echo "= MESSAGE: "$message" "
 total=$((total + 1))
-output=$(timeout "$TIMEOUT"s bash -c "nostril --envelope --sec '$privkey' --content '${message//\'/\'\\\'\'}' | websocat $relay")
+output=$(timeout "$TIMEOUT"s bash -c "nostril --envelope --sec '$privkey' --content '${message//\'/\'\\\'\'}' --pow "$POW" | websocat $relay")
+source ./SORT.sh
 source ./COUNT.sh
-echo "
 
-
-
-
-"
 done
 fi
 
