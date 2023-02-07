bd115cb35f50cea3f9106926c9fa70081b41ff9a
gourcetools <120996278+gourcetools@users.noreply.github.com>
2023-02-07 04:54:47 +0100

    Split makejson and broadcast-infos

---
+++ src/scripts/broadcast-profile-infos/broadcast-profile-infos.sh
@@ -0,0 +1,55 @@
+echo "  ===============================================  "
+echo "  This script will broadcast profiles informations"
+echo "  for ALL accounts that you generated."
+echo "  It may take some times as it will try-again until  "
+echo "  ALL accounts infos are succesfully send. "
+echo "  ===============================================  "
+echo "  -Please be patient if it seems stuck, it's not- "
+echo "  ===============================================  "
+
+echo "  == Enter the domain you used for NIP-05 file:"
+echo "  == Leave blank to broadcast without NIP-05 info "
+echo "  == Example: gourcetools.github.io"
+read domain
+
+# Start timer
+start_time=$(date +%s)
+
+cd ../../keys/pubkeys
+
+num_cores=$(nproc)
+ls *.txt | xargs -P $num_cores -I {} bash -c '
+  file={}
+  name=${file%.txt}
+  if [ -z "$domain" ]; then
+    FULLNIP=" "
+  else
+    FULLNIP="$name@$domain"
+  fi
+  privkey=$(cat ../privkeys/$file)
+  pubkey=$(cat ../pubkeys/$file)
+  relay=wss://relay.nostr.band
+  while true; do
+    echo "  == 🚀 Broadcasting profile informations for: $name to: $relay "
+    result=$(timeout 5s nostril --kind 0 --envelope --sec "$privkey" --content "{\"name\":\"$name\",\" picture\":\"https://nostr.build/i/5616.jpeg\",\"nip05\":\"$FULLNIP\"}" | websocat "$relay")
+    if echo "$result" | grep -q "true"; then
+      break
+    fi
+    echo "  == 😞 Broadcast failed. We will try again for: $name "
+    relay=$(shuf -n 1 ../../../config/relays-list.txt)
+  done
+  echo "  == 😃 DONE broadcasting profile informations for: $name "
+'
+
+ # End timer
+end_time=$(date +%s)
+# Calculate total time
+total_time=$((end_time - start_time))
+totalacc=$(ls ../../keys/pubkeys | wc -l) 
+echo " "
+ echo "  ===============================================  "
+echo " ✔️  BROADCASTED ALL PROFILES INFORMATIONS SUCCESULLY ✔️ "
+echo " 🔃 WE UPDATED $totalacc PROFILES IN "$total_time" SECONDS. 🔁"
+ echo "  ===============================================  "
+ 
+ cd ../../scripts/broadcast-profile-infos
--- src/scripts/make-json-nip05/makejson.sh
+++ src/scripts/make-json-nip05/makejson.sh
@@ -1,9 +1,3 @@
-#!/bin/bash
-# Read the user input   
-echo "Enter the domain where you will be hosting the nostr.json file: "  
-echo "example : gourcetools.github.io"
-read domain  
-
 # Create json directory
 mkdir -p ../../json
 
@@ -20,28 +14,23 @@ cd ../../keys/pubkeys
 for file in *.txt;
 do 
 	name=${file%.txt}
-	privkey=$(cat ../privkeys/$file)
-	pubkey=$(cat ../pubkeys/$file)
-  	relay=$(shuf -n 1 ../../../config/relays-list.txt)
 	(
 		flock -x 200
 		echo -ne " \"$name\" : \"$(cat $file)\"," >> ../../json/nostr.json
 	) 200>/tmp/nostr_json.lock & 
  	
- 	echo "== Broadcasting NIP05 for $name "
-	timeout 5s nostril --kind 0 --envelope --sec "$privkey" --content "{\"name\":\"$name\",\" picture\":\"https://nostr.build/i/5616.jpeg\",\"nip05\":\"$name@$domain\"}" | websocat "$relay" &
+ 	echo "== Adding $name to json "
 done
 wait
 
 
-rm -f log.txt
 truncate -s -1 ../../json/nostr.json
 echo -e "}"						>> ../../json/nostr.json
 echo -e "}"						>> ../../json/nostr.json
 ## Finish writing nostr.json 
 
 # Navigate and echo instructions 
-cd ../../scripts
+cd ../../scripts/make-json-nip05
 echo "== JSON FILE READY TO UPLOAD "
-echo "== UPLOAD IT TO "$domain"/.well.known/nostr.json "
-echo "== JSON FILE is located at spamster/src/json/nostr.json "
+echo "== UPLOAD IT TO YOUR-WEBSITE.COM/.well.known/nostr.json "
+echo "== THE JSON FILE is located at spamster/src/json/nostr.json "
