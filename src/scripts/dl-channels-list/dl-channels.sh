0e5765a07b454156d583d2b1aecc48b6c93605e3
gourcetools <120996278+gourcetools@users.noreply.github.com>
2023-02-07 01:58:18 +0100

    Switch default indexer to relay.nostr.band

--- src/scripts/dl-channels-list/dl-channels.sh
+++ src/scripts/dl-channels-list/dl-channels.sh
@@ -1,3 +1,3 @@
-echo '["REQ","RAND", {"kinds": ['40'],"limit": '100'}]' | nostcat wss://relay.nostr.ch |
+echo '["REQ","RAND", {"kinds": ['40'],"limit": '1000'}]' | nostcat wss://relay.nostr.band |
   	jq '.[2].id' > ../../../config/channels-list.txt
     sed -i 's/^.\(.*\).$/\1/' ../../../config/channels-list.txt
