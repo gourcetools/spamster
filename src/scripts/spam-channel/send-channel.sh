3e7ec44b737c8b83b5c05cd636726e996885d8a3
gourcetools <120996278+gourcetools@users.noreply.github.com>
2023-02-08 01:55:49 +0100

    Remove comment used for testing

--- src/scripts/spam-channel/send-channel.sh
+++ src/scripts/spam-channel/send-channel.sh
@@ -14,6 +14,5 @@ echo "     MESSAGE:	""$message""							"
 echo "============================================================================== 	"
 echo " "
 nostril --envelope --sec "$privkey" --kind 42 --tag e "$line" --content "$message" | websocat $relay
-# nostril --envelope --kind 42 --tag e 25e5c82273a271cb1a840d0060391a0bf4965cafeb029d5ab55350b418953fbb --content "send from CLI using nostril" | websocat wss://relay.nostr.ch
 done < ../../../config/channels-list.txt
 cd ../scripts/spam-channel
