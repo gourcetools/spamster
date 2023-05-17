9e9c3b45c4463b63e22fd079188151f3c9e885d6
dhermiboats <120996278+gourcetools@users.noreply.github.com>
2023-05-17 20:28:26 +0200

    Update reset.sh

--- spamster/src/scripts/reset/reset.sh
+++ spamster/src/scripts/reset/reset.sh
@@ -1,58 +1,54 @@
+#!/bin/bash
 
+# Function to delete directory content safely
+safe_delete () {
+    echo " =  🗑️  Deleting content in $1"
+    find "$1" -type f -exec rm -f {} +
+}
+
+# User confirmation for deletion
 while true; do
-  read -p "
+  read -rp "
     == Are you sure you want to delete:
     == ALL: accounts,relays,nostr.json files? 
     == Answer: [y/n] " choice
-  if [[ $choice == "y" || $choice == "Y" ]]; then
-    echo " == Oki doki..."
-    sleep 1
-    # continue with the rest of the script
-    break
-  elif [[ $choice == "n" || $choice == "N" ]]; then
-    echo " == I knew it :P "
-    sleep 1
-    source ../menu/START.sh
-    break
-  else
-    echo " == Invalid option. Please enter y or n."
-  fi
+
+  case $choice in
+    [yY]* )
+      echo " == Oki doki..."
+      sleep 1
+      break;;
+    [nN]* )
+      echo " == I knew it :P "
+      sleep 1
+      source ../menu/START.sh
+      break;;
+    * )
+      echo " == Invalid option. Please enter y or n.";;
+  esac
 done
 
-echo " "
-echo " "
-echo " ============================="
+echo -e "\n\n ============================="
 echo " == SPAMSTER RESET STARTED == "
 echo " ============================="
-cd ../../
-echo " = Purging tmp folder"
-cd tmp
-rm -f *
-cd ..
-echo " =  🗑️ Deleting stuff in pubkeys folder"
-cd keys/pubkeys
-rm -f *
-cd ..
-echo " =  🗑️ Deleting stuff in privkeys folder"
-cd privkeys
-rm -f *
-echo " =  🗑️ Deleting nostr.json"
-cd ../../json
-rm -f *
-cd ../../config
-echo " =  🗑️ Deleting contact-list.csv"
-rm -f contact-list.csv
-echo " =  🗑️ Deleting privkeys-list.txt"
-rm -f privkeys-list.txt
-echo " =  🗑️ Deleting relays-list.txt"
-rm -f relays-list.txt
 
-cd ../src/scripts/reset
+# Start deleting files
+safe_delete ../../tmp
+safe_delete ../../keys/pubkeys
+safe_delete ../../keys/privkeys
+safe_delete ../../json
+# Deleting specific files
+echo " =  🗑️ Deleting specific files"
+rm -f ../../../config/contact-list.csv
+rm -f ../../../config/privkeys-list.txt
+rm -f ../../../config/relays-list.txt
 
-echo " =  🗑️ Deleting multi-spamster"
-rm -f -d -r ../../../../multi-spamster
+# Deleting multi-spamster directory
+echo " =  🗑️ Deleting multi-spamster directory"
+rm -rf ../../../../multi-spamster
 
 echo " ============================="
 echo " == 🚮 SPAMSTER RESET DONE 🚮 == "
 echo " ============================="
-    sleep 1
+
+sleep 1
